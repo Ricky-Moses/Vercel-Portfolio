3c14f6171fa80d614dcfe667d2c64cd9ca15120c
Ricky-Prophet <studya86308@gmail.com>
2025-05-04 09:35:11 +0530

    Sunday Commit

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 
-const head = "Hello there, I'm Ricky Moses"
+const head = "Hello There, I'm Ricky Moses"
 const sub = "Automation is not the goal — it is the standard. I stylize with surgical precision and deploy with mission-grade stability. I transform logic into landscapes, interactions into outcomes, and data into design."
 const headContent = head.split('')
 const subContent = sub.split('')
 console.log(headContent);
 console.log(subContent);
+
 const Home = () => {
   const [subTime, setSubTime] = useState(false)
+  const [mediaTime, setMediaTime] = useState(false)
 
   useEffect(() => {
     const time = setTimeout(() => {
@@ -15,15 +17,34 @@ const Home = () => {
     }, 6000)
     return () => clearTimeout(time)
   })
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setMediaTime(true)
+    }, 6500)
+    return () => clearTimeout(time)
+  })
   return (
     <>
-      <div className="home flex items-center justify-center">
-        <main className="md:max-w-3/4 h-1/4 text-center">
+      <div className="home flex flex-col items-center justify-center">
+        <main className="md:max-w-3/4 h-3/6 flex flex-col items-center gap-2 text-center">
           <div className="head">
             {headContent.map((text, i) => <h1 key={i} className={`title title-${i} tracking-widest inline font-bold`}>{text}</h1>)}
           </div>
           <div className="sub">
-            {subTime && subContent.map((text, i) => <div key={i} className={`content content-${i} inline font-bold`}>{text}</div>)}
+            {subTime && subContent.map((text, i) => <div key={i} className={`content content-${i} inline font-bold tracking-widest`}>{text}</div>)}
+          </div>
+          <div className="contact flex flex-col items-center gap-3">
+            {mediaTime && <>
+              <h1 className="font-bold underline">Touch With Me</h1>
+              <div className="link flex items-center justify-center gap-5 sm:gap-10 lg:gap-20">
+                <a href='' className=""><div className="items">IN</div></a>
+                <a href='' className=""><div className="items">FB</div></a>
+                <a href='' className=""><div className="items">GH</div></a>
+                <a href='' className=""><div className="items">LI</div></a>
+                <a href='' className=""><div className="items">GM</div></a>
+              </div>
+            </>}
           </div>
         </main>
       </div>
