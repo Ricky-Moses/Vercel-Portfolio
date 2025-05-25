7a6ccad88b5c6fc465c7564a92ee3e6d39e21d68
Ricky-Prophet <studya86308@gmail.com>
2025-05-25 20:59:30 +0530

    From Start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,9 @@
-import React, { useEffect, useState } from "react";
-// Router
-import { Routes, Route, useNavigate } from 'react-router-dom'
-// Utilities
-import Open from "./components/Utilities/Open";
-import Swipe from "./components/Utilities/Swipe";
-// Layouts
-import Header from "./components/Layouts/Header";
-// Pages
-import Home from "./components/pages/Home";
-import About from './components/pages/About';
-import Skill from './components/pages/Skill';
-import Project from './components/pages/Project';
-import Contact from './components/pages/Contact';
+import React from 'react'
 
 const App = () => {
-  const [showIntro, setShowIntro] = useState(false); // For Developing need to - {true}
-  const [isFade, setIsFade] = useState(false);
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (showIntro) {
-      const fadeTimer = setTimeout(() => {
-        setIsFade(true);
-      }, 3500);
-
-      const introTimer = setTimeout(() => {
-        setShowIntro(false);
-      }, 4100);
-
-      return () => {
-        clearTimeout(fadeTimer);
-        clearTimeout(introTimer);
-      };
-    }
-  }, [showIntro, navigate]);
   return (
-    <>
-    <Swipe>
-      {showIntro ? (<Open fadingOut={isFade} />) : (
-        <Routes>
-          <Route path="/" element={<Header />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="skill" element={<Skill />} />
-            <Route path="project" element={<Project />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      )}
-    </Swipe>
-    </>
+    <div>App</div>
   )
-};
+}
 
-export default App;
+export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 // Sass
 import './main.scss'
-// React Router
-import { HashRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </StrictMode>
 )
