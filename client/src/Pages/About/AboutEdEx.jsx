a2ec6996fa9677efc933d8c150b3f8e8800a698e
Ricky-Prophet <studya86308@gmail.com>
2025-05-30 22:36:01 +0530

    vide config

--- client/src/Pages/About/AboutEdEx.jsx
+++ client/src/Pages/About/AboutEdEx.jsx
@@ -30,18 +30,20 @@ const AboutMe = [
         place: 'SDLC',
         desc: 'Web development today feels dynamic and fast-paced, blending creativity with logic.I enjoy building responsive, interactive apps using tools like React, Node.js, and MongoDB.It\'s a continuous learning journey where each project helps me grow and improve.'
     }
-    
+
 ]
 
 const AboutEdEx = () => {
     return (
-        <div className="about-ed grid place-items-center gap-6 !p-3">
+        <section className="about-ed grid place-items-center gap-6 !p-3">
             <div className="">
                 <h1 className="font-bold">Experience & Education</h1>
             </div>
             <div className="w-11/12 md:w-3/4 grid lg:grid-cols-2 gap-8 place-items-center ">
-                {AboutMe?.map(({Icon, year, studies, place, desc}) =>(
-                    <div className="w-full h-full flex gap-2 border-l border-gray-500">
+                {AboutMe?.map(({ Icon, year, studies, place, desc }, idx) => (
+                    <div
+                        key={idx}
+                        className="w-full h-full flex gap-2 border-l border-gray-500">
                         <div className="indicator !p-3">
                             <span className="w-8 h-8 indicator-item indicator-start badge badge-success text-2xl text-white">{Icon}</span>
                         </div>
@@ -57,7 +59,7 @@ const AboutEdEx = () => {
                     </div>
                 ))}
             </div>
-        </div>
+        </section>
     )
 }
 
